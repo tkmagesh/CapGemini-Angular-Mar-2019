40b4863cce24ef7b516b48776a14c91cedb279a1
Magesh <tkmagesh77@gmail.com>
2019-03-15 10:18:03 +0530

    greeter - part - 1

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GreeterComponent } from './greeter/greeter.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    , GreeterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+  	AppComponent
+  ]
 })
 export class AppModule { }
--- first-app/src/main.ts
+++ first-app/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import calc from './calculator';
+
+console.log(calc);
+
 if (environment.production) {
   enableProdMode();
 }
