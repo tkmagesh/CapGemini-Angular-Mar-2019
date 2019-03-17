b0c69be22051e93c28d185ee5bf1e1fb4ab6e83f
Magesh <tkmagesh77@gmail.com>
2019-03-17 12:38:15 +0530

    using events

--- async-demos/async-demos.js
+++ async-demos/async-demos.js
@@ -14,21 +14,44 @@
 
 	window['addSyncClient'] = addSyncClient;
 
-	function addAsync(x,y){
+	function addAsync(x,y, callback){
 		console.log(`	[@Service] processing ${x} and ${y}`);
 		setTimeout(function(){
 			var result = x + y;
 			console.log(`	[@Service] returning result`);
-			return result;
+			callback(result);
 		}, 4000);
 	}
 
 	function addAsyncClient(x,y){
 		console.log(`[@Client] triggering addAsync`);
-		var result = addAsync(x,y);
-		console.log(`[@Client] result = ${result}`)
+		addAsync(x,y, function(result){
+			console.log(`[@Client] result = ${result}`)
+		});
 	}
 
 	window['addAsyncClient'] = addAsyncClient;
+
+	var addAsyncEvents = (function(){
+		var _listeners = [];
+
+		function subscribe(listenerFn){
+			_listeners.push(listenerFn);
+		}
+
+		function process(x,y){
+			console.log(`	[@Service] processing ${x} and ${y}`);
+			setTimeout(function(){
+				var result = x + y;
+				console.log(`	[@Service] returning result`);
+				_listeners.forEach(listenerFn => listenerFn(result));
+			}, 4000);
+		}
+
+		return { process, subscribe };
+
+	})();
+
+	window['addAsyncEvents'] = addAsyncEvents;
 
 })();
